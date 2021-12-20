f16699b52883c1db0459280e9c02073e34d98243
Msolc98 <lillit0728@gmail.com>
2021-12-20 22:43:42 +0900

    main 세부 UI 수정 main,signup,storeselect intent 연결

--- app/src/main/java/com/example/project_3f4c68/MainActivity.kt
+++ app/src/main/java/com/example/project_3f4c68/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.project_3f4c68
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener({
+            var intent = Intent(this,StoreSelctActivity::class.java)
+            startActivity(intent)
+            })
+
+        signup_button.setOnClickListener({
+            var intent = Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
